36f9f7b89a4adadbad4ed2050eba9f5ada6c887e
Marius Schär <schaer.marius@gmail.com>
2017-05-27 02:19:59 +0200

    added permalinking

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
 <html>
-<?php readfile("resources/components/head.html")?>
+<?php include("resources/components/head.php");?>
 <body>
-	<?php readfile("resources/components/header.html")?>
+	<?php readfile("resources/components/header.html");?>
 	<div class="page-wrap">
+		<div name="firstpost" class="card">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla blandit rutrum libero vel accumsan. Vestibulum et congue tortor. Morbi ullamcorper ipsum ac augue accumsan, vel porta leo condimentum. Vestibulum a imperdiet mauris, id tristique diam. Curabitur id aliquet erat, sed condimentum nisl. Aenean urna velit, luctus vel ligula in, ullamcorper scelerisque sem. Donec eget felis ut elit commodo auctor vel a nisi. Donec feugiat viverra massa, sed venenatis dui volutpat nec. Cras at tortor euismod, congue enim elementum, tincidunt risus.
+		
+			Aliquam id diam et mi blandit vulputate. In ac est cursus nulla blandit dignissim a quis metus. Suspendisse suscipit nibh mi, ut convallis ipsum malesuada non. Duis rutrum massa ut iaculis hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam pharetra dolor at dui placerat, non rhoncus ex malesuada. Fusce consectetur augue eu ex elementum tincidunt. Cras posuere dapibus mollis. Suspendisse feugiat dapibus purus. Aenean nec magna mattis, fermentum lacus non, aliquet tellus. Ut id neque condimentum, vehicula nisl et, euismod urna.
 
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla blandit rutrum libero vel accumsan. Vestibulum et congue tortor. Morbi ullamcorper ipsum ac augue accumsan, vel porta leo condimentum. Vestibulum a imperdiet mauris, id tristique diam. Curabitur id aliquet erat, sed condimentum nisl. Aenean urna velit, luctus vel ligula in, ullamcorper scelerisque sem. Donec eget felis ut elit commodo auctor vel a nisi. Donec feugiat viverra massa, sed venenatis dui volutpat nec. Cras at tortor euismod, congue enim elementum, tincidunt risus.
+			Nullam non sem sit amet augue ornare pellentesque. Aliquam erat volutpat. Quisque metus dolor, semper ac condimentum sit amet, rhoncus non ligula. Curabitur suscipit cursus mollis. Vestibulum id varius mauris. Sed et mauris non lectus molestie ullamcorper. Ut auctor, metus eget molestie pulvinar, nisl odio mollis eros, id rutrum mauris nibh et sem. Maecenas convallis at tellus at sagittis. Proin ornare condimentum nisl, eget iaculis massa semper ullamcorper.
 
-		Aliquam id diam et mi blandit vulputate. In ac est cursus nulla blandit dignissim a quis metus. Suspendisse suscipit nibh mi, ut convallis ipsum malesuada non. Duis rutrum massa ut iaculis hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam pharetra dolor at dui placerat, non rhoncus ex malesuada. Fusce consectetur augue eu ex elementum tincidunt. Cras posuere dapibus mollis. Suspendisse feugiat dapibus purus. Aenean nec magna mattis, fermentum lacus non, aliquet tellus. Ut id neque condimentum, vehicula nisl et, euismod urna.
-
-		Nullam non sem sit amet augue ornare pellentesque. Aliquam erat volutpat. Quisque metus dolor, semper ac condimentum sit amet, rhoncus non ligula. Curabitur suscipit cursus mollis. Vestibulum id varius mauris. Sed et mauris non lectus molestie ullamcorper. Ut auctor, metus eget molestie pulvinar, nisl odio mollis eros, id rutrum mauris nibh et sem. Maecenas convallis at tellus at sagittis. Proin ornare condimentum nisl, eget iaculis massa semper ullamcorper.
-
-		In nisl velit, posuere auctor arcu a, cursus vestibulum nulla. Phasellus ac blandit tellus, sit amet molestie dui. Pellentesque eu ante nibh. Aliquam erat volutpat. Morbi risus ipsum, facilisis id tellus sed, congue porttitor orci. In hac habitasse platea dictumst. Aliquam erat volutpat. Donec feugiat bibendum ante, auctor mattis dui placerat vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum eu risus consectetur, vehicula ipsum a, tempus eros. Pellentesque pharetra sagittis lorem, ac lacinia sem. Morbi at hendrerit ex.
-
-		Donec luctus mauris ac lorem porttitor aliquet. Quisque vitae feugiat justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum faucibus accumsan mi id maximus. Sed cursus iaculis nisi sed volutpat. Morbi in nulla viverra, pellentesque risus a, viverra nibh. Morbi nec molestie justo, quis gravida lectus. In venenatis pellentesque ipsum, nec rutrum orci porta vitae. Curabitur iaculis purus sapien. Sed feugiat convallis velit. Maecenas nunc velit, maximus in dapibus eu, ornare nec nulla. In hac habitasse platea dictumst.
+			In nisl velit, posuere auctor arcu a, cursus vestibulum nulla. Phasellus ac blandit tellus, sit amet molestie dui. Pellentesque eu ante nibh. Aliquam erat volutpat. Morbi risus ipsum, facilisis id tellus sed, congue porttitor orci. In hac habitasse platea dictumst. Aliquam erat volutpat. Donec feugiat bibendum ante, auctor mattis dui placerat vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum eu risus consectetur, vehicula ipsum a, tempus eros. Pellentesque pharetra sagittis lorem, ac lacinia sem. Morbi at hendrerit ex.
+		</div>
 	</div>
-	<?php readfile("resources/components/footer.html")?>
+	<?php readfile("resources/components/footer.html");?>
 </body>
 </html>
 <!--
@@ -36,5 +35,6 @@ Char table:
 	&#xf08c;
 	&#xf0f4;
  	&#xf08e;
+	&#xf0c1;
 Surround in <span class="fa">...</span>
 -->
